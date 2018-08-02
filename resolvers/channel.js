b8b995acaca80866be6c87a18b19ff416f4ffc5d
Your Name <dre.murguido@gmail.com>
2018-08-01 21:57:40 -0400

    refactored out directMessaging with private channels

--- resolvers/channel.js
+++ resolvers/channel.js
@@ -8,40 +8,64 @@ export default {
   Mutation: {
     getOrCreateChannel: requiresAuth.createResolver(
       async (parent, { members, teamId }, { models, user }) => {
-        members.push(user.id);
+        const member = await models.Member.findOne(
+          { where: { teamId, userId: user.id } },
+          { raw: true }
+        );
+        if (!member) {
+          throw new Error("You are not a member");
+        }
+
+        const allMembers = [...members, user.id];
+
         //check if channel exists with members
         // array_agg(pc.user_id) <- concats into array pc.user_id
         const [data, result] = await models.sequelize.query(
           `
-          select c.id 
+          select c.id, c.name 
           from channels as c, pcmembers pc
           where pc.channel_id = c.id and c.dm = true and c.public = false and c.team_id = ${teamId}
-          group by c.id 
-          having array_agg(pc.user_id) @> Array[${members.join(
+          group by c.id, c.name 
+          having array_agg(pc.user_id) @> Array[${allMembers.join(
             ","
-          )}] and count(pc.user_id) = ${members.length}
+          )}] and count(pc.user_id) = ${allMembers.length}
         `,
           { raw: true }
         );
-        console.log("DATA: ", data, "RESULT: ", result);
+        console.log(data, result);
         if (data.length) {
-          return data[0].id;
+          return data[0];
         }
+
+        const users = await models.User.findAll({
+          raw: true,
+          where: { id: { [models.sequelize.Op.in]: members } }
+        });
+
+        const name = users.map(u => u.username).join(", ");
+
         const channelId = await models.sequelize.transaction(
           async transaction => {
             const channel = await models.Channel.create(
-              { name: "HEllo", public: false, dm: true, teamId },
+              { name, public: false, dm: true, teamId },
               {
                 transaction
               }
             );
             const cId = channel.dataValues.id;
-            const pcmembers = members.map(m => ({ userId: m, channelId: cId }));
+            const pcmembers = allMembers.map(m => ({
+              userId: m,
+              channelId: cId
+            }));
+            console.log(channel);
             await models.PCMember.bulkCreate(pcmembers, { transaction });
             return cId;
           }
         );
-        return channelId;
+        return {
+          id: channelId,
+          name
+        };
       }
     ),
     createChannel: requiresAuth.createResolver(
--- schema/channel.js
+++ schema/channel.js
@@ -15,8 +15,12 @@ export default `
     type Query {
         allChannels: [Channel!]!
     }
+    type DMChannelResponse {
+        id: Int!
+        name: String!
+    }
     type Mutation {
         createChannel(teamId: Int!, name: String!, public: Boolean=false, members: [Int!]): ChannelResponse
-        getOrCreateChannel(teamId: Int!, members: [Int!]!): Int!
+        getOrCreateChannel(teamId: Int!, members: [Int!]!): DMChannelResponse!
     }
 `;
